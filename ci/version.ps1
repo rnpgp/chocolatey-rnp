0f5207ca8727ac0a0079f108c535ce89a30ff0e3
Roman Shevchenko <roman.shevchenko@millix.org>
2020-02-02 15:30:42 +0300

    path fix

--- ci/version.ps1
+++ ci/version.ps1
@@ -1,4 +1,4 @@
-$node = Select-Xml -XPath "/package/metadata/version" -Path ${GITHUB_WORKSPACE}/rnp/rnp.nuspec
+$node = Select-Xml -XPath "/package/metadata/version" -Path ${env:GITHUB_WORKSPACE}/rnp/rnp.nuspec
 $version = $node.Node.InnerText
 Write-Output "Determined RNP version to build: $version"
 $Env:VERSION = $version
