2f0d0f8249bdacfb4342867422072a23aa6814f7
Roman Shevchenko <roman.shevchenko@millix.org>
2020-02-02 15:48:31 +0300

    update

--- ci/version.ps1
+++ ci/version.ps1
@@ -1,4 +1,6 @@
-$node = Select-Xml -XPath "/package/metadata/version" -Path ${env:GITHUB_WORKSPACE}/rnp/rnp.nuspec
+$Namespace = @{nuspec = "http://schemas.microsoft.com/packaging/2015/06/nuspec.xsd"}
+$Path = "${env:GITHUB_WORKSPACE}/rnp/rnp.nuspec"
+$node = Select-Xml -Path $Path -Namespace $Namespace -XPath "//nuspec:package/nuspec:metadata/nuspec:version"
 $version = $node.Node.InnerText
 Write-Output "Determined RNP version to build: $version"
-$Env:VERSION = $version
+$env:VERSION = $version
