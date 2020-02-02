47ac9c2ff7ccdd246df5426aa666fcdb15d2da92
Roman Shevchenko <rrrooommmaaa@mail.ru>
2020-02-02 14:27:24 +0300

    checking out correct version of RNP source

--- ci/main.sh
+++ ci/main.sh
@@ -5,13 +5,13 @@ CMAKE=cmake
 
 cmakeopts=(
   "-DCMAKE_BUILD_TYPE=Release"
-  "-DBUILD_SHARED_LIBS=yes"
+  "-DBUILD_SHARED_LIBS=no"
   "-DCMAKE_INSTALL_PREFIX=${RNP_INSTALL}"
   "-G" "MSYS Makefiles"
 )
 
 mkdir -p "${LOCAL_BUILDS}/rnp-build"
-rnpsrc="$PWD"
+rnpsrc="$HOME/rnpsrc"
 pushd "${LOCAL_BUILDS}/rnp-build"
 
 # update dll search path for windows
