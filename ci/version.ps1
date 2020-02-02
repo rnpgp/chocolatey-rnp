af2b99f13ae906855911cc971e4925dcacc26071
Roman Shevchenko <roman.shevchenko@millix.org>
2020-02-02 16:23:06 +0300

    fix

--- ci/version.ps1
+++ ci/version.ps1
@@ -2,5 +2,4 @@ $Namespace = @{nuspec = "http://schemas.microsoft.com/packaging/2015/06/nuspec.x
 $Path = "${env:GITHUB_WORKSPACE}/rnp/rnp.nuspec"
 $node = Select-Xml -Path $Path -Namespace $Namespace -XPath "//nuspec:package/nuspec:metadata/nuspec:version"
 $version = $node.Node.InnerText
-Write-Output "Determined RNP version to build: $version"
-$env:VERSION = $version
+Write-Output $version
